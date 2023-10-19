0a6f04b4f225a879e08a362808fc2b199539b719
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-10-19 16:23:51 -0300

    Add files via upload

---
+++ TKINTER/FIRSTSWINDOW.py
@@ -0,0 +1,34 @@
+#funções da lib são atribuídas 
+import tkinter as tk 
+#import qrcode #pip install qrcode
+#from PIL import Image, ImageDraw #pip install pillow
+#from tkinter import messagebox
+from tkinter import*
+
+#_____________________________________________________#
+
+#atribui a variavel a WINDOW a uma função
+window = tk.Tk()
+
+#atribui um nome a janela criada
+window.title("My first window")
+
+window.resizable(False, True)
+
+#troca cor da janela
+window.configure(bg="red", borderwidth="5")
+window.geometry("300x1000")
+
+
+#gui=Graphic User Interface
+gui = Tk(className="workout with TkInter")
+
+#configurar tamanha da janela
+gui.geometry("500x2000")
+gui.configure(bg="red")
+
+
+#executa a variavel window(JANELA)
+window.mainloop()
+
+#_____________________________________________________#
