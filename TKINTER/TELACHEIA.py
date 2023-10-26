19b482a335cb305d79a763f0e862db7dce3b36bd
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-10-26 15:06:57 -0300

    Add files via upload

---
+++ TKINTER/TELACHEIA.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import font
+
+window = tk.Tk()
+window.geometry("500x500")
+window.title("Redimensionando Janela")
+window["bg"]= '#fff'
+
+label_font = font.Font(size=60)
+label = tk.Label(window, wraplength=300, font=label_font, text="")
+label.pack()
+
+def atualizar_window():
+
+    window.update()
+    window.update_idletasks()
+    print(f'Janela atualizada com sucesso!')
+
+label= tk.Label(window, text="Pressione [ESC] para fechar a janela")
+
+label.pack()
+
+window.bind("<Escape>", lambda e, w=window: w.destroy())
+
+
+def botao_clique():
+    window.destroy()
+
+botao = tk.Button(window, text="Fechar Janela", command=botao_clique)
+botao.pack()
+
+
+window.state("zoomed")
+
+window.mainloop()
